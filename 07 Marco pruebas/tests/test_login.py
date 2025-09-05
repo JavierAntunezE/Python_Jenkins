ff7e5e82ef4089a4d65e712d92e936941176b9a1
Javier <javier.ant.4@gmail.com>
2025-09-05 16:08:15 -0600

    Actualiza credenciales

--- 07 Marco pruebas/tests/test_login.py
+++ 07 Marco pruebas/tests/test_login.py
@@ -1,4 +1,3 @@
-# archivo: test_login.py
 import time
 import unittest
 from selenium import webdriver
@@ -6,8 +5,6 @@ from selenium import webdriver
 from pages.login_page import LoginPage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#from selenium.webdriver.common.by import By
-#from utils.db_helper import Database
 
 class TestLogin(unittest.TestCase):
 
@@ -17,12 +14,6 @@ class TestLogin(unittest.TestCase):
         self.driver.get("https://www.saucedemo.com/")
         self.driver.maximize_window()
         self.login_page = LoginPage(self.driver)
-        #self.db = Database(
-        #    server="localhost",
-        #    database="TestLoginDB",
-        #    username="userPythonDB",
-        #    password="Abc.123"
-        #)
 
     def config_options(self):
         self.options = webdriver.ChromeOptions()
@@ -30,24 +21,20 @@ class TestLogin(unittest.TestCase):
              "profile.password_manager_leak_detection": False
         }
         self.options.add_experimental_option("prefs", prefs)
-        self.options.add_argument('--headless')
+        #self.options.add_argument('--headless')
 
     def test_login_valido(self): #Test de login exitoso
-        #user = self.db.get_user("standard_user")  # Devuelve ("admin", "admin123")
-        #username, password = user
-
         self.login_page.wait_until_loaded()
         self.login_page.enter_username("standard_user")
-        self.login_page.enter_password("password")
+        self.login_page.enter_password("secret_sauce")
         self.login_page.click_login()
 
         # Espera explícita a que cargue la página de inventario
-        WebDriverWait(self.driver, 5).until(
+        WebDriverWait(self.driver, 10).until(
             EC.url_contains("inventory.html"))
 
     def tearDown(self): #Se ejecuta después de cada función
         time.sleep(0.5)
-        #self.db.close()
         self.driver.quit()
 
 if __name__ == "__main__": #Solo se ejecuta si el archivo se corre directamente
